d801e4e34f92e835e83573a700237ff098e6cfe3
Your Name <damaris.golea@gmail.com>
2019-06-05 21:12:30 +0300

    studied classes, variables, data types

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,16 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    int doorCount;
+    double mileage;
+    String color;
+    double maxSpeed;
+    boolean running;
+    double fuelLevel;
+    double traveledDistance;
+
+    // "has-a" relationship
+    Engine engine;
+}
